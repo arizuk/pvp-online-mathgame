fee8c5bb067870f99140c431b28001dcceb05008
masaki.arizuka <masaki.arizuka@gmail.com>
2020-04-12 22:12:26 +0900

    Remove setupProxy

--- frontend/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const {createProxyMiddleware} = require("http-proxy-middleware")
-console.log("setupProxy rqeuired")
-
-module.exports = app => {
-    app.use('/ws', createProxyMiddleware({ target: 'ws://127.0.0.1:8000/ws', ws: true }))
-}
